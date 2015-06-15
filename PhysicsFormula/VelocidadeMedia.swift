a5a2b075ce538e822b243f9ddb01e4446c271059
thiagocoradi <thiagocoradi@gmail.com>
2015-06-15 14:58:10 -0300

    FuncaoHorariaDoDeslocamento
    to Lucas

--- PhysicsFormula/VelocidadeMedia.swift
+++ PhysicsFormula/VelocidadeMedia.swift
@@ -10,20 +10,4 @@ import UIKit
 
 class VelocidadeMedia: UIView {
 
-    @IBOutlet weak var label: UILabel!
-    /*
-    
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-    @IBAction func changeAgain(sender: AnyObject) {
-    
-    label.text = "Thiago"
-    
-    }
-
-    @IBOutlet weak var change: UIButton!
 }
