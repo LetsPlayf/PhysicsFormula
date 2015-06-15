270108b15ff3a0c6ae686d63fbcaca98a97997bf
ironzep <renangeraldo@MacBook-Pro-de-Renan.local>
2015-06-15 15:20:36 -0300

    commit saveLucas

--- PhysicsFormula/VelocidadeMedia.swift
+++ PhysicsFormula/VelocidadeMedia.swift
@@ -9,5 +9,26 @@
 import UIKit
 
 class VelocidadeMedia: UIView {
+    @IBOutlet weak var deltaS: UITextField!
+    @IBOutlet weak var deltaT: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
 
+    
+    
+    
+    @IBAction func calcular(sender: AnyObject) {
+    var deltaSDobule = (deltaS.text as NSString).doubleValue
+    var deltaTDouble = (deltaT.text as NSString).doubleValue
+        
+        var  result : Double = deltaSDobule/deltaTDouble
+        
+    resultLabel.text = String(stringInterpolationSegment: result)
+        
+    }
+    @IBAction func clean(sender: AnyObject) {
+    
+    deltaS.text = nil
+    deltaT.text = nil
+
+    }
 }
