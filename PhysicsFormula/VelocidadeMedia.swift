349141d8c254cecae1366c7c1f724296173ce973
ironzep <renangeraldo@MacBook-Pro-de-Renan.local>
2015-06-16 13:13:31 -0300

    velocidadeMedia class
    To Thiago

--- PhysicsFormula/VelocidadeMedia.swift
+++ PhysicsFormula/VelocidadeMedia.swift
@@ -12,23 +12,41 @@ class VelocidadeMedia: UIView {
     @IBOutlet weak var deltaS: UITextField!
     @IBOutlet weak var deltaT: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
-
+    
     
     
     
     @IBAction func calcular(sender: AnyObject) {
-    var deltaSDobule = (deltaS.text as NSString).doubleValue
-    var deltaTDouble = (deltaT.text as NSString).doubleValue
         
-        var  result : Double = deltaSDobule/deltaTDouble
         
-    resultLabel.text = String(stringInterpolationSegment: result)
+        if(deltaT.text == "0" || deltaT.text == "" || deltaS.text == "" || deltaS.text == "0"){
+            
+            let alert = UIAlertView()
+            alert.title = "Erro"
+            alert.message = "Insira um número válido"
+            alert.addButtonWithTitle("OK")
+            alert.show()
+            
+            
+            
+        }else{
+            var deltaSDobule = (deltaS.text as NSString).doubleValue
+            var deltaTDouble = (deltaT.text as NSString).doubleValue
+            
+            
+            var  result : Double = deltaSDobule/deltaTDouble
+            
+            
+            
+            resultLabel.text = String(format: "%.2f",result)
+        }
         
     }
-    @IBAction func clean(sender: AnyObject) {
     
-    deltaS.text = nil
-    deltaT.text = nil
-
+    @IBAction func clean(sender: AnyObject) {
+        
+        deltaS.text = nil
+        deltaT.text = nil
+        
     }
 }
