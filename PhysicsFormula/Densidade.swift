0965a5adf6b9ea314e51c7188b6901e29bbbaf6a
ironzep <renangeraldo@MacBook-Pro-de-Renan.local>
2015-06-17 18:39:48 -0300

    commit

--- PhysicsFormula/Densidade.swift
+++ PhysicsFormula/Densidade.swift
@@ -9,32 +9,65 @@
 import UIKit
 
 class Densidade: UIView {
-    @IBOutlet weak var f: UITextField!
+    
 
-    @IBOutlet weak var a: UITextField!
-   
+    @IBOutlet weak var m: UITextField!
+    @IBOutlet weak var v: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
+  
     @IBOutlet weak var cleanButton: UIButton!
+
     @IBOutlet weak var resultButton: UIButton!
 
-    
     override func willMoveToSuperview(newSuperview: UIView?){
         cleanButton.layer.cornerRadius = 0.09 * cleanButton.bounds.size.width
         resultButton.layer.cornerRadius = 0.09 * resultButton.bounds.size.width
     }
+
+    
     
     @IBAction func calculate(sender: AnyObject) {
+        if(m.text == "0" || v.text == "0" || m.text == "" || v.text == ""){
+            
+            let alert = UIAlertView()
+            alert.title = "Erro"
+            alert.message = "Insira um número válido"
+            alert.addButtonWithTitle("OK")
+            alert.show()
+            
+            
+            
+        }else{
+            
+            let mPonto = m.text.stringByReplacingOccurrencesOfString(",", withString: ".", options: NSStringCompareOptions.LiteralSearch, range: nil)
+            
+            let vPonto = v.text.stringByReplacingOccurrencesOfString(",", withString: ".", options: NSStringCompareOptions.LiteralSearch, range: nil)
+            
+            var mDobule = (mPonto as NSString).doubleValue
+            var vDouble = (vPonto as NSString).doubleValue
+            
+            
+            var  result : Double = mDobule / vDouble
+            
+            
+            
+            resultLabel.text = String(format: "%.2f",result) + " g/cm\u{00B3}"
+        }
+
+    
     
     
     
     }
-    
+
     @IBAction func clean(sender: AnyObject) {
     
-    
+        m.text = nil
+        v.text = nil
+        
+        resultLabel.text = String(format: "%.2f",0)
     
     
     }
-    
-}
    
+}
